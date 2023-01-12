383ae1733acf630c3a9232ad1b267c78c939a243
HJY0 <gripix324@gmail.com>
2023-01-12 15:48:34 +0900

    first commit

---
+++ client/js/part01.js
@@ -0,0 +1,72 @@
+
+
+
+const data = [
+  {
+    "id": 1,
+    "src": "visual1.jpg",
+    "alt": "모던한 테이블과 화분의 조화를 표현한 공간"
+  },
+  {
+    "id": 2,
+    "src": "visual2.jpg",
+    "alt": "강렬한 의자의 색상과 따뜻한 느낌의 공간"
+  },
+  {
+    "id": 3,
+    "src": "visual3.jpg",
+    "alt": "호텔 라운지 느낌의 편안한 의자가 있는 공간"
+  },
+  {
+    "id": 4,
+    "src": "visual4.jpg",
+    "alt": "물방을 모양의 독특한 디자인의 의자들을 나열한 공간"
+  }
+]
+
+
+const navigation = getNode('.navigation');
+// const list = getNodes('.navigation > li');
+const visualImage = getNode('.visual img');
+
+// 유사 배열을 진짜 배열로 만드는 유틸함수
+function makeArray(arrayLike) {
+  return Array.from(arrayLike);
+}
+
+
+function handler(e){
+  let target = e.target.closest('li');
+  if(!target) return;
+
+  let list = makeArray(navigation.children);
+  let index = attr(target,'data-index');
+
+
+  Array.from(list);
+  list.forEach(item => removeClass(item,'is-active'));
+
+  attr(visualImage,'src',`./assets/part01/visual${index}.jpg`);
+  attr(visualImage,'alt',data[index-1].alt);
+
+  addClass(target,'is-active');
+}
+
+navigation.addEventListener('click',handler);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ client/js/part04.js
@@ -0,0 +1,77 @@
+const contents = getNode('.contents');
+const textField = getNode('#comment37');
+
+
+function clearText(target) {
+  target.value = "";
+}
+
+
+function handler(e){
+  let target = e.target;
+  
+
+
+  while(!attr(target,'data-name')){ // 부모 찾는 반복문
+    target = target.parentNode;
+
+    if(target.nodeName === 'BODY'){ // 부모 찾다가 부모가 BODY가 될 때 끝내라
+      target = null;
+      return;
+    }
+  }
+
+
+  if(target.dataset.name === 'like'){
+    console.log('like');
+    toggleClass(target,'active')
+  }
+
+
+  if(target.dataset.name === 'comment'){
+    textField.focus();
+  }
+  
+  
+  if(target.dataset.name === 'send'){
+    console.log(textField.value);
+
+    let template = /* html */ `
+    <div class="id">
+      <div class="profile_img border_over">
+        <img src="./assets/part03/oh.png" alt="">
+      </div>
+        <div class="comment_field">
+          <div class="text_container">
+            <div class="name"><a href="#">홍자영</a></div>
+            <div class="text_field">${textField.value}</div>
+        </div>
+      </div>
+    </div>
+    `;
+
+    insertLast('.comment_container',template);
+    clearText(textField);
+  }
+
+
+}
+
+
+contents.addEventListener('click',handler)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
