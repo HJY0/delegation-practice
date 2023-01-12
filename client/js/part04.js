0daf1afa765a84417c37f6b7e3346ccebc79b900
HJY0 <gripix324@gmail.com>
2023-01-12 15:49:22 +0900

    키보드 엔터 입력 추가

--- client/js/part04.js
+++ client/js/part04.js
@@ -63,7 +63,27 @@ contents.addEventListener('click',handler)
 
 
 
+textField.addEventListener('keydown',(e)=>{
 
+  if(e.keyCode === 13){
+    textField.value
+
+    let template = `
+      <div class="id">
+      <div class="profile_img border_over"><img src="./assets/part03/tiger.png" alt=""></div>
+          <div class="comment_field">
+              <div class="text_container">
+                  <div class="name"><a href="#">심선범</a></div>
+                  <div class="text_field">${textField.value}</div>
+              </div>
+          </div>
+      </div>
+      `
+      insertLast('.comment_container',template);
+
+      clearText(textField)
+  }
+})
 
 
 
